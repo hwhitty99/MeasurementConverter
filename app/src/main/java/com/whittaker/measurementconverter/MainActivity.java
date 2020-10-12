66f5ed8d6efb165d21f5a13e53674895c1e4b783
Hudson <thewhittakers6@gmail.com>
2020-10-12 16:07:39 -0500

    Completed XML layout and declared private Java variables

--- app/src/main/java/com/whittaker/measurementconverter/MainActivity.java
+++ app/src/main/java/com/whittaker/measurementconverter/MainActivity.java
@@ -3,12 +3,34 @@ package com.whittaker.measurementconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener{
+
+    private TextView unit1Label;
+    private TextView unit2Labe;
+    private EditText unit1EditText;
+    private TextView unit2TextView;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton1;
+    private RadioButton radioButton2;
+    private RadioButton radioButton3;
+    private RadioButton radioButton4;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
